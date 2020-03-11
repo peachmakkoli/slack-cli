bba4b68b3e1d695b38507a6b6c5b9085390078c8
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-10 17:08:36 -0700

    creates Workspace class and tests for instantiation

---
+++ test/workspace_test.rb
@@ -0,0 +1,22 @@
+require_relative 'test_helper'
+
+describe "Workspace class" do
+	before do
+		@workspace = Workspace.new
+	end
+
+	describe "Workspace instantiation" do
+		it "is an instance of Workspace" do
+			expect(@workspace).must_be_kind_of Workspace
+		end
+
+		it "is set up for specific attributes and data types" do
+      [:users, :channels].each do |attribute|
+        expect(@workspace).must_respond_to attribute
+      end
+
+      expect(@workspace.users).must_be_kind_of Array
+			expect(@workspace.channels).must_be_kind_of Array
+		end
+	end
+end
