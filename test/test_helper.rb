29a763b9f11549181b95f38a2dcd7d2a22c0a2b7
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-10 17:10:27 -0700

    requires Workspace lib and filters Slack token

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,8 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'vcr'
 
+require_relative '../lib/workspace'
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
@@ -25,5 +27,7 @@ VCR.configure do |config|
   }
 
   # Don't leave our token lying around in a cassette file.
-
+  config.filter_sensitive_data("<SLACK_TOKEN>") do
+    ENV["SLACK_TOKEN"]
+  end
 end
