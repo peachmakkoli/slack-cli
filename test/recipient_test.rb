17549bca17603e0df78a3cb0a383d8af28e36c48
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-12 16:15:19 -0700

    adds constants and nominal tests for #self.get

--- test/recipient_test.rb
+++ test/recipient_test.rb
@@ -1,6 +1,17 @@
 require_relative 'test_helper'
 
 describe "Recipient class" do
+	API_KEY = ENV["SLACK_TOKEN"]
+	BASE_URL = "https://slack.com/api/"
+	POST_URL = "#{BASE_URL}chat.postMessage"
+	GETUSER_URL = "#{BASE_URL}users.list"
+	GETCHANNEL_URL = "#{BASE_URL}conversations.list"
+	GET_QUERY = {
+		query: {
+			token: API_KEY,
+		}
+	}
+
 	before do
 		@recipient = SlackCLI::Recipient.new(
 			slack_id: "UV7S06J4X", 
@@ -20,6 +31,24 @@ describe "Recipient class" do
 
       expect(@recipient.slack_id).must_be_kind_of String
 			expect(@recipient.name).must_be_kind_of String
+		end
+	end
+
+	describe "#self.get" do
+		it "can get a list of users" do
+			VCR.use_cassette("slack-users") do
+				response = SlackCLI::Recipient.get(GETUSER_URL, GET_QUERY)
+				expect(response.code).must_equal 200
+				expect(response.parsed_response["ok"]).must_equal true
+			end
+		end
+
+		it "can get a list of channels" do
+			VCR.use_cassette("slack-channels") do
+				response = SlackCLI::Recipient.get(GETCHANNEL_URL, GET_QUERY)
+				expect(response.code).must_equal 200
+				expect(response.parsed_response["ok"]).must_equal true
+			end
 		end
 	end
 end
