9b9731210ac963315e023ef002fcdac767667b6c
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 19:52:45 -0700

    adds User class and tests instantiation, error with unknown keywords

---
+++ lib/user.rb
@@ -0,0 +1,15 @@
+require_relative 'recipient'
+
+module SlackCLI
+	class User < Recipient
+		attr_reader :real_name, :status_text, :status_emoji
+
+		def intitialize(slack_id:, name:, real_name:, status_text:, status_emoji:)
+			super(slack_id, name)
+			@real_name = real_name
+			@status_text = status_text
+			@status_emoji = status_emoji
+		end
+		
+	end
+end
---
+++ test/user_test.rb
@@ -0,0 +1,31 @@
+require_relative 'test_helper'
+
+describe "User class" do
+	before do
+		@user = SlackCLI::Recipient::User.new(
+			slack_id: "UV7S06J4X", 
+			name: "Lee",
+			real_name: "Lee Higgins",
+			status_text: "I'm happy :)",
+			status_emoji: ":sparkles:"
+		)
+	end
+
+	describe "User instantiation" do
+		it "is an instance of User" do
+			expect(@user).must_be_kind_of SlackCLI::Recipient::User #check this!!!!!! thank you faezeh :)))
+		end
+
+		it "is set up for specific attributes and data types" do
+      [:slack_id, :name, :real_name, :status_text, :status_emoji].each do |attribute|
+        expect(@user).must_respond_to attribute
+      end
+
+      expect(@user.slack_id).must_be_kind_of String
+			expect(@user.name).must_be_kind_of String
+			expect(@user.real_name).must_be_kind_of String
+			expect(@user.status_text).must_be_kind_of String
+			expect(@user.status_emoji).must_be_kind_of String
+		end
+	end
+end
