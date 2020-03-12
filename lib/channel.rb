40fb3032fe636667dc5fb8116b0b3f89de0a5503
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-12 16:23:47 -0700

    adds pseudocode and assigns the url for get requests to a constant

--- lib/channel.rb
+++ lib/channel.rb
@@ -2,6 +2,8 @@ require_relative 'recipient'
 
 module SlackCLI
 	class Channel < Recipient
+		GETCHANNEL_URL = "#{SlackCLI::Recipient::BASE_URL}conversations.list"
+
 		attr_reader :topic, :member_count
 
 		def initialize(slack_id:, name:, topic:, member_count:)
@@ -11,11 +13,11 @@ module SlackCLI
 		end
 
 		def details
-
+			# return the attributes of a channel
 		end
 
 		def self.list_all
-			
+			# call the self.get method with the GETCHANNEL_URL, pass all relevant data from response into the initializer
 		end
 		
 	end
--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,8 @@ require_relative 'recipient'
 
 module SlackCLI
 	class User < Recipient
+		GETUSER_URL = "#{SlackCLI::Recipient::BASE_URL}users.list"
+
 		attr_reader :real_name, :status_text, :status_emoji
 
 		def initialize(slack_id:, name:, real_name:, status_text:, status_emoji:)
@@ -12,11 +14,11 @@ module SlackCLI
 		end
 
 		def details
-			
+			# return the attributes of a channel
 		end
 
 		def self.list_all
-			
+			# call the get method, pass all relevant data from response into the initializer
 		end
 		
 	end
