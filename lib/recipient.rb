32756b3d430c3855e20bcd1ad4d4b550132201d0
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 21:38:01 -0700

    add signatures for implemented and abstract methods

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -7,5 +7,23 @@ module SlackCLI
 			@name = name
 		end
 		
+		def send_message(message)
+
+		end
+
+		def self.get(url, params)
+
+		end
+
+		private 
+
+		def details
+			raise NotImplementedError, 'Implement me in a subclass!'
+		end
+
+		def self.list_all
+			raise NotImplementedError, 'Implement me in a subclass!'
+		end
+
 	end
 end
