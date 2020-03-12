13752b4b2cb877be44b779037dd357028a668c29
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 19:53:12 -0700

    adds Channel class and tests instantiation, error with unknown keywords

---
+++ lib/channel.rb
@@ -0,0 +1,14 @@
+require_relative 'recipient'
+
+module SlackCLI
+	class Channel < Recipient
+		attr_reader :topic, :member_count
+
+		def intitialize(slack_id:, name:, topic:, member_count:)
+			super(slack_id, name)
+			@topic = topic["value"]
+			@member_count = member_count
+		end
+		
+	end
+end
---
+++ test/channel_test.rb
@@ -0,0 +1,33 @@
+require_relative 'test_helper'
+
+describe "Channel class" do
+	before do
+		@channel = SlackCLI::Channel.new(
+			slack_id: "CUT6YR3LJ", 
+			name: "lees-test-channel",
+			topic: {
+				"value": "Testing the Slack CLI",
+				"creator": "",
+				"last_set": 0
+			},
+			member_count: 2
+		)
+	end
+
+	describe "Channel instantiation" do
+		it "is an instance of Channel" do
+			expect(@channel).must_be_kind_of SlackCLI::Channel
+		end
+
+		it "is set up for specific attributes and data types" do
+      [:slack_id, :name, :topic, :member_count].each do |attribute|
+        expect(@channel).must_respond_to attribute
+      end
+
+      expect(@channel.slack_id).must_be_kind_of String
+			expect(@channel.name).must_be_kind_of String
+			expect(@channel.topic).must_be_kind_of String
+			expect(@channel.member_count).must_be_kind_of Integer
+		end
+	end
+end
