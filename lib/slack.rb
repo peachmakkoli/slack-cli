fdff433bbbb5f31629c5892cfaba13911032adeb
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-14 22:33:20 -0700

    adds helper methods for select user and select channel

--- lib/slack.rb
+++ lib/slack.rb
@@ -20,7 +20,31 @@ def main
 
   def list_channels
   return @workspace.channels
-  end
+	end
+	
+	def select_user_option
+		puts "Please enter the user ID:"
+		id = gets.chomp.upcase
+		begin
+			@workspace.select_user(id)
+		rescue SlackAPIError => error
+			puts error.message
+		else
+			puts "Ok! \"#{@workspace.selected.name}\" selected."
+		end
+	end
+
+	def select_channel_option
+		puts "Please enter the channel ID:"
+		id = gets.chomp.upcase
+		begin
+			@workspace.select_channel(id)
+		rescue SlackAPIError => error
+			puts error.message
+		else
+			puts "Ok! \"#{@workspace.selected.name}\" selected."
+		end
+	end
 
   loop do
 		puts "\nWhat would you like to do? \n1) list users \n2) list channels \n3) select user \n4) select channel \n5) details \n6) quit"
@@ -32,28 +56,16 @@ def main
 			when "2", "list channels"
 				tp list_channels, :slack_id, :name, :topic, :member_count
 			when "3", "select user"
-				puts "Please enter the user ID:"
-				id = gets.chomp.upcase
-				begin
-					tp @workspace.select_user(id), :slack_id, :name, :real_name, :status_text, :status_emoji
-				rescue SlackAPIError => error
-					puts error.message
-				end
+				select_user_option
 			when "4", "select channel"
-				puts "Please enter the channel ID:"
-				id = gets.chomp.upcase
-				begin
-					tp @workspace.select_channel(id), :slack_id, :name, :topic, :member_count
-				rescue SlackAPIError => error
-					puts error.message
-				end
+				select_channel_option
 			when "5", "details"
 				# When I type details, the program should print out details for the currently selected recipient. What information is printed depends on whether it's a channel or a user.
 				# If no recipient is currently selected, the program should let me know and return to the main command prompt.
 			when "6", "quit"
 				break
 			else 
-				puts "Option not recognized! Please try again."
+				puts "Sorry, that's not an option!"
 		end
 	end
 
